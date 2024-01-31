794dbf1b9e1bb56331fe93f2c22b7c9bb18a50df
Daniel San <danielgdt88@hotmail.com>
2024-01-31 19:18:09 -0300

    att

--- script/scripst.js
+++ script/scripst.js
@@ -9,3 +9,32 @@ document.addEventListener("DOMContentLoaded", function () {
     botao.style.display = botao.style.display === "none" ? "block" : "none";
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.querySelector(".carousel");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+
+  let currentIndex = 0;
+  const totalSlides = document.querySelectorAll(".carousel img").length;
+
+  prevBtn.addEventListener("click", function () {
+    showSlide(currentIndex - 1);
+  });
+
+  nextBtn.addEventListener("click", function () {
+    showSlide(currentIndex + 1);
+  });
+
+  function showSlide(index) {
+    if (index < 0) {
+      index = totalSlides - 1;
+    } else if (index >= totalSlides) {
+      index = 0;
+    }
+
+    const translateValue = -index * (100 / totalSlides) + "%";
+    carousel.style.transform = "translateX(" + translateValue + ")";
+    currentIndex = index;
+  }
+});
